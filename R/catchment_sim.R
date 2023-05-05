14f80eaf69adbc8222817d28ff4df2da2746df31
Vinay Joshy <80487875+unmaskedcrusader@users.noreply.github.com>
2023-05-05 13:31:23 -0400

    minor change

--- R/catchment_sim.R
+++ R/catchment_sim.R
@@ -6,7 +6,7 @@
 #'
 #' @param n number of observations to be simulated
 #' @param alpha shape parameter of gamma distribution
-#' @param gamma rate parameter of gamma distribution
+#' @param beta rate parameter of gamma distribution
 #' @param area square dimension for catchment (if area = 20, then each catchment dimensions will be 20 x 20)
 #'
 #' @return data frame of n observations
@@ -15,10 +15,10 @@
 #' @examples catch_df <- catchment_sim(16, 4.313320, 3.026894, 20)
 #'
 #'
-catchment_sim <- function(n, alpha, gamma, area){
+catchment_sim <- function(n, alpha, beta, area){
 
   #gamma distribution of catchment area
-  size <- round(stats::rgamma(n, alpha, gamma)) |>
+  size <- round(stats::rgamma(n, alpha, beta)) |>
     (\(x) replace(x, x == 0, 1))()
 
 
