588a03e529d2cd1ed16fc5e277f1b1618b4bd342
Vinay Joshy <80487875+unmaskedcrusader@users.noreply.github.com>
2023-05-05 13:52:16 -0400

    added elementary_pop function

--- R/elementary_pop.R
+++ R/elementary_pop.R
@@ -1,9 +1,24 @@
 
-elementary_pop <- function(df, shape, rate){
+#' Create Elementary Schools population size
+#'
+#' Function to simulation elementary school size and then assigned to catchment areas.
+#'
+#' @param df output data frame from catchment_sim function
+#' @param alpha shape parameter of gamma distribution
+#' @param beta rate parameter of gamma distribution
+#'
+#' @return a data frame of simulated elementary school population
+#' @export
+#'
+#' @examples catch_df <- catchment_sim(16, 4.313320, 3.026894, 20)
+#' elementary_df <- elementary_pop(catch_df, 5.27426341, 0.01427793)
+#'
+#'
+elementary_pop <- function(df, alpha, beta){
 
 
   # gamma distribution of number of elementary schools
-  school_pop <- round(stats::rgamma(sum(df$num.schools), shape, rate))
+  school_pop <- round(stats::rgamma(sum(df$num.schools), alpha, beta))
   school_id <- seq.int(length(school_pop))
 
 
--- tests/testthat/test-catchment_sim.R
+++ tests/testthat/test-catchment_sim.R
@@ -1,11 +1,20 @@
 
+#simulate catchment area
+catch_df <- catchment_sim(16, 4.313320, 3.026894, 20)
+
+#simulate elementary schools for each area
+elementary_df <- elementary_pop(catch_df, 5.27426341, 0.01427793)
+
+catch_id <- aggregate(elementary_df$schoolPop, FUN = "sum", by = list(elementary_df$catchID))[,1]
+
+
 test_that("catchment_sim creates a data frame of 16 rows", {
-  expect_equal(dim(catchment_sim(16, 4.313320, 3.026894, 20)), c(16,6))
+  expect_equal(dim(catch_df), c(16,6))
 })
 
-
-test_that("catchment_sim creates a data frame of 20 rows", {
-  expect_equal(dim(catchment_sim(20, 4.313320, 3.026894, 20)), c(20,6))
+test_that("elementary schools are assigned to catchment areas", {
+  expect_equal(catch_df$catchID, catch_id)
 })
+
 
 
