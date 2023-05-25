86c2e97ccb9882519b1cf6fab6eeec7cd4b3bfd5
unmaskedcrusader <vjoshy98@gmail.com>
2023-05-25 13:59:15 +0530

    have to update and test simepi function

--- R/simepi.R
+++ R/simepi.R
@@ -1,36 +1,54 @@
-library(EpiILM)
-library(splitstackshape)
+#' Simulate epidemic
+#'
+#' simulate epidemic
+#'
+#' @param df dataframe
+#' @param b parameter
+#' @param sus parameter
+#' @param spar parameter
+#' @param num_inf parameter
+#'
+#' @importFrom dplyr %>%
+#'
+#' @return list of simulated epidemic
+#' @export
+#'
+#' @examples
+#' \dontrun{whaever }
+sim12epiNew <- function(df, b, sus, spar, num_inf){
 
-sim12epiNew <- function(b, sus, spar, num_inf){
+  catchID <- individualID <- loc.x <- loc.y <- NULL
 
   runs <- list()
-  num_catchment <- length(unique(individuals$catchID))
+  num_catchment <- length(unique(df$catchID))
 
   for(i in 1:10){
 
     # Select individuals from each catchment area for initial infections.
     #   Number of initial infections per cathcment area = numinf.catch
-    sample <- stratified(individuals, "catchID", size = num_inf)$individualID
+    sample <- (df %>%
+                 dplyr::group_by(catchID) %>%
+                 dplyr::slice_sample(n = 2))$individualID
 
     # Set Initial infection time for the sampled individuals
     #   Start is the "epidemic start time"
     #   Individuals are randomly infected within 14 days after the start time
-    first.inf <- rep(0, nrow(individuals))
-    start <- max(round(rnorm(1, mean = 45, sd=15)), 20)
-    first.inf[individuals$individualID %in% sample] <- start + floor(runif(num_inf*num_catchment, 0, 15))
+    first.inf <- rep(0, nrow(df))
+    start <- max(round(stats::rnorm(1, mean = 45, sd=15)), 20)
+    first.inf[df$individualID %in% sample] <- start + floor(stats::runif(num_inf*num_catchment, 0, 15))
 
     # simulate epidemic
-    SIR <- epidata(type="SIR"
-                   , n=nrow(individuals)
+    SIR <- EpiILM::epidata(type="SIR"
+                   , n=nrow(df)
                    , tmin=1
                    , tmax=270
                    , sus.par= sus
                    , beta= b
                    , spark = spar
-                   , x=individuals$loc.x*2
-                   , y=individuals$loc.y*2
+                   , x=df$loc.x*2
+                   , y=df$loc.y*2
                    , inftime = first.inf
-                   , infperiod=rep(4, nrow(individuals)))
+                   , infperiod=rep(4, nrow(df)))
 
 
     # calculate infection rate by taking ratio of sum of infection time to
@@ -39,17 +57,17 @@ sim12epiNew <- function(b, sus, spar, num_inf){
 
     if(infection.rate <= 0.02){
 
-      SIR <- epidata(type="SIR"
-                     , n=nrow(individuals)
+      SIR <- EpiILM::epidata(type="SIR"
+                     , n=nrow(df)
                      , tmin=1
                      , tmax=270
                      , sus.par= sus
                      , beta= b
                      , spark = spar
-                     , x=individuals$loc.x*2
-                     , y=individuals$loc.y*2
+                     , x=df$loc.x*2
+                     , y=df$loc.y*2
                      , inftime = first.inf
-                     , infperiod=rep(4, nrow(individuals)))
+                     , infperiod=rep(4, nrow(df)))
     }
 
 
