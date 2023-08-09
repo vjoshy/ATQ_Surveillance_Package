42e20f7ce80287def73ec91c1dbe300c054c8c54
Vinay Joshy <80487875+unmaskedcrusader@users.noreply.github.com>
2023-08-09 12:44:17 -0400

    added metrics list and description

---
+++ R/metrics.R
@@ -0,0 +1,23 @@
+#' Metrics Data
+#'
+#' @name metrics
+NULL
+
+#' Simulated catchment area data
+#'
+#' A data frame simulated from code catchment_sim(16, 0.5, 1.5, 5)
+#'
+#'  \itemize{
+#'    \item catchID, ID associated with each catchment
+#'    \item num.schools, Number of schools in each catchment
+#'    \item xStart, Start x coordinate of catchment grid
+#'    \item xEnd, End x coordinate of catchment grid
+#'    \item yStart, Start y coordinate of catchment grid
+#'    \item yEnd, End y coordinate of catchment grid
+#'}
+#'
+#' @docType data
+#' @name catchment
+#' @usage data(catchment)
+#' @format A data frame with 16 rows and 6 variables
+NULL
