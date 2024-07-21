edcba0c9968c5429cb42d2d0c806833bf97f0649
Vinay Joshy <80487875+unmaskedcrusader@users.noreply.github.com>
2024-07-20 21:29:44 -0400

    minor fixes to description and namespace

--- R/plot.alarm_metrics.R
+++ R/plot.alarm_metrics.R
@@ -6,13 +6,14 @@
 #' @param x An object of class "alarm_metrics" containing matrices of metric values.
 #' @param metric A character string specifying which metric to plot.
 #'        Default is "AATQ". Options include "FAR", "ADD", "AATQ", "FATQ", "WAATQ", "WFATQ".
+#' @param col Set heat map color profile, default is heat.colors(15)
 #' @param ... Additional arguments passed to the image function.
 #'
 #' @return A heatmap plot of the specified metric.
 #' @export
 #'
 #' @importFrom graphics image
-#'
+#' @importFrom grDevices heat.colors
 #' @examples
 #' # Generate sample alarm metrics data
 #' set.seed(123)
@@ -28,7 +29,9 @@
 #'   AATQ = generate_metric_matrix(),
 #'   FATQ = generate_metric_matrix(),
 #'   WAATQ = generate_metric_matrix(),
-#'   WFATQ = generate_metric_matrix()
+#'   WFATQ = generate_metric_matrix(),
+#'  lags = 1:15,
+#'  thresholds = seq(0.1, 0.6, by = 0.05)
 #' )
 #'
 #' # Create an alarm_metrics object
@@ -43,11 +46,22 @@
 #' # Customize the plot
 #' plot(alarm_metrics_obj, metric = "FATQ", col = heat.colors(12))
 #'
-plot.alarm_metrics <- function(x, metric = "AATQ", ...) {
+plot.alarm_metrics <- function(x, metric = "AATQ", col = heat.colors(12), ...) {
+  # Check if 'col' is provided in ...
+  dots <- list(...)
+  if ("col" %in% names(dots)) {
+    col <- dots$col
+    dots$col <- NULL
+  }
 
   # Create a heatmap of the chosen metric
-  image(z = x[[metric]], x = x$lags, y = x$thresholds,
-        main = paste(metric, "by lag and threshold"),
-        xlab = 'Lags', ylab = 'Thresholds',
-        col = heat.colors(15), ...)
+  do.call(image, c(list(
+    z = x[[metric]],
+    x = x$lags,
+    y = x$thresholds,
+    main = paste(metric, "by lag and threshold"),
+    xlab = 'Lags',
+    ylab = 'Thresholds',
+    col = col
+  ), dots))
 }
